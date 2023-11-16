b692a4218d8476a9f1922f56f3e81492bcecf1a2
hieu <soulclean2001@gmail.com>
2023-11-16 15:22:40 +0700

    fix notify resume potential

--- Backend/JS/src/services/resume.services.ts
+++ Backend/JS/src/services/resume.services.ts
@@ -65,18 +65,23 @@ class ResumeService {
       if (oldUrls.length === 1) await deleteFileFromS3(oldUrls[0])
     }
 
-    const listFollower = await this.getListFollowers(resume_id)
-    const recievers = listFollower.map((id) => id.toString())
+    const recievers = await this.getListFollowers(resume_id)
+    const recievers2 = await this.getListPotentials(resume_id, user_id)
 
     const candidate = await databaseServices.candidate.findOne({
       user_id: new ObjectId(user_id)
     })
 
+    const jobapplied = await databaseServices.jobApplication.findOne({
+      user_id: new ObjectId(user_id),
+      cv_id: new ObjectId(resume_id)
+    })
+
     await NotificationService.notify(
       {
-        content: `Ứng viên tiềm năng ${
+        content: `1 ứng viên mà bạn đang theo dõi '${
           oldResume?.user_info?.first_name + ' ' + oldResume?.user_info?.last_name
-        } đã cập nhật lại CV của mình`,
+        }' đã cập nhật lại CV của mình`,
         object_recieve: NotificationObject.Employer,
         object_sent: NotificationObject.Candidate,
         recievers,
@@ -88,10 +93,24 @@ class ResumeService {
       }
     )
 
+    await NotificationService.notify(
+      {
+        content: `1 ứng viên tiềm năng gần đây đã nộp đơn: '${jobapplied?.full_name}' đã cập nhật lại CV của mình`,
+        object_recieve: NotificationObject.Employer,
+        object_sent: NotificationObject.Candidate,
+        recievers: recievers2,
+        type: 'potential/update',
+        sender: new ObjectId(user_id)
+      },
+      {
+        candidate_id: candidate?._id
+      }
+    )
+
     return result.value
   }
 
-  static async getListFollowers(cvId: string): Promise<ObjectId[]> {
+  static async getListFollowers(cvId: string) {
     const followers = await databaseServices.candidate
       .aggregate([
         {
@@ -118,11 +137,128 @@ class ResumeService {
             follower_ids: 1,
             _id: 0
           }
+        },
+        {
+          $unwind: {
+            path: '$follower_ids',
+            preserveNullAndEmptyArrays: true
+          }
+        },
+        {
+          $lookup: {
+            from: 'company',
+            localField: 'follower_ids',
+            foreignField: '_id',
+            as: 'company'
+          }
+        },
+        {
+          $unwind: {
+            path: '$company',
+            preserveNullAndEmptyArrays: true
+          }
+        },
+        {
+          $addFields: {
+            users: '$company.users'
+          }
+        },
+        {
+          $project: {
+            users: 1
+          }
         }
       ])
       .toArray()
 
-    return followers[0]?.follower_ids || []
+    const userIds: string[] = []
+    for (let i = 0; i < followers.length; i++) {
+      for (let j = 0; j < followers[i].users.length; j++) {
+        userIds.push(followers[i].users[j].user_id.toString())
+      }
+    }
+
+    return userIds
+  }
+
+  static async getListPotentials(cvId: string, userId: string) {
+    const potentials = await databaseServices.jobApplication
+      .aggregate([
+        {
+          $match: {
+            user_id: new ObjectId(userId),
+            status: 3,
+            cv_id: new ObjectId(cvId)
+          }
+        },
+        {
+          $lookup: {
+            from: 'jobs',
+            localField: 'job_post_id',
+            foreignField: '_id',
+            as: 'job'
+          }
+        },
+        {
+          $unwind: {
+            path: '$job',
+            preserveNullAndEmptyArrays: true
+          }
+        },
+        {
+          $lookup: {
+            from: 'company',
+            localField: 'job.company_id',
+            foreignField: '_id',
+            as: 'company'
+          }
+        },
+        {
+          $unwind: {
+            path: '$company',
+            preserveNullAndEmptyArrays: true
+          }
+        },
+        {
+          $group: {
+            _id: '$company._id'
+          }
+        },
+        {
+          $lookup: {
+            from: 'company',
+            localField: '_id',
+            foreignField: '_id',
+            as: 'company'
+          }
+        },
+        {
+          $unwind: {
+            path: '$company',
+            preserveNullAndEmptyArrays: true
+          }
+        },
+        {
+          $addFields: {
+            users: '$company.users'
+          }
+        },
+        {
+          $project: {
+            users: 1
+          }
+        }
+      ])
+      .toArray()
+
+    const userIds: string[] = []
+    for (let i = 0; i < potentials.length; i++) {
+      for (let j = 0; j < potentials[i].users.length; j++) {
+        userIds.push(potentials[i].users[j].user_id.toString())
+      }
+    }
+
+    return userIds
   }
 
   static async updateOrCreateResume(user_id: string, payload: ResumeRequestBody) {
@@ -163,6 +299,49 @@ class ResumeService {
       }
       if (oldUrls.length === 1) await deleteFileFromS3(oldUrls[0])
     }
+
+    const recievers = await this.getListFollowers(result.value._id.toString())
+    const recievers2 = await this.getListPotentials(result.value._id.toString(), user_id)
+
+    const candidate = await databaseServices.candidate.findOne({
+      user_id: new ObjectId(user_id)
+    })
+
+    const jobapplied = await databaseServices.jobApplication.findOne({
+      user_id: new ObjectId(user_id),
+      cv_id: new ObjectId(result.value._id.toString())
+    })
+
+    await NotificationService.notify(
+      {
+        content: `1 ứng viên mà bạn đang theo dõi '${
+          oldResume?.user_info?.first_name + ' ' + oldResume?.user_info?.last_name
+        }' đã cập nhật lại CV của mình`,
+        object_recieve: NotificationObject.Employer,
+        object_sent: NotificationObject.Candidate,
+        recievers,
+        type: 'resume/update',
+        sender: new ObjectId(user_id)
+      },
+      {
+        candidate_id: candidate?._id
+      }
+    )
+
+    await NotificationService.notify(
+      {
+        content: `${jobapplied?.full_name}`,
+        object_recieve: NotificationObject.Employer,
+        object_sent: NotificationObject.Candidate,
+        recievers: recievers2,
+        type: 'potential/update',
+        sender: new ObjectId(user_id)
+      },
+      {
+        job_applied_id: jobapplied?._id
+      }
+    )
+
     return result.value
   }
 
