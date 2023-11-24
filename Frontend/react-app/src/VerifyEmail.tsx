e33d52231569e9af3d09d35bad19b8837172aca9
Phong <phongpt2@imt-soft.com>
2023-11-24 22:03:40 +0700

    fe

--- Frontend/react-app/src/VerifyEmail.tsx
+++ Frontend/react-app/src/VerifyEmail.tsx
@@ -9,7 +9,7 @@ import { decodeToken } from './utils/jwt'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { AppThunkDispatch, useAppDispatch } from './app/hook'
-import { useDispatch } from 'react-redux'
+// import { useDispatch } from 'react-redux'
 
 export const VerifyEmail = () => {
   const [message, setMessage] = useState('')
