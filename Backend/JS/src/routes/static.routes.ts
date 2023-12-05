262c5500c1f64f635cf88b0a4d67e87b7086adb7
hieu <soulclean2001@gmail.com>
2023-12-05 13:06:00 +0700

    fix code

--- Backend/JS/src/routes/static.routes.ts
+++ Backend/JS/src/routes/static.routes.ts
@@ -5,8 +5,5 @@ import wrapAsync from '~/utils/handlers'
 const staticRouter = Router()
 
 staticRouter.get('/image/:name', wrapAsync(mediasControllers.serveImage))
-staticRouter.get('/video-stream/:name', wrapAsync(mediasControllers.serveVideoStream))
-staticRouter.get('/video-hls/:id/master.m3u8', wrapAsync(mediasControllers.serveM3u8))
-staticRouter.get('/video-hls/:id/:v/:segment', wrapAsync(mediasControllers.serveSegment))
 
 export default staticRouter
